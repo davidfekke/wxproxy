b4873646fe4e7bbe79d17b7338c9137b131f1a9b
David Fekke <david@fekke.com>
2019-04-18 11:56:00 -0400

    Adding cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const axios = require('axios');
 const parser = require('fast-xml-parser');
+const cors = require('cors');
 
 const port = process.env.PORT || '3000';
 const app = express();
+
+app.use(cors())
 
 app.get('/metar/:icaoidentifier', (req, res) => {
     axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=${req.params.icaoidentifier}&hoursBeforeNow=2`).then(xml => {
