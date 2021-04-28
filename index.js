f03e1b37de89d856a3af83c6b9fe3ec6e2fc8324
David Fekke <david@fekke.com>
2021-04-27 21:04:24 -0400

    Adding bug fix to last route

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
-const express = require('express');
-const axios = require('axios');
-const parser = require('fast-xml-parser');
-const cors = require('cors');
+import Fastify from 'fastify';
+import fastifyCors from 'fastify-cors';
+import axios from 'axios';
+import * as parser from 'fast-xml-parser';
+
+const fastify = Fastify({ logger: true });
 
 const port = process.env.PORT || '3000';
-const app = express();
 
-app.use(cors())
-
-app.get('/metar/:icaoidentifier', (req, res) => {
-    axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=${req.params.icaoidentifier}&hoursBeforeNow=2`).then(xml => {
-        const jsonObj = parser.parse(xml.data);
-        if (jsonObj.response.data.METAR.length > 0) {
-            res.json(jsonObj.response.data.METAR);    
-        } else {
-            res.json(jsonObj.response);
-        }
-    }).catch(err => {
-        res.json(err);
-    });
+fastify.register(fastifyCors, { 
+    // put your options here
 });
 
-app.get('/taf/:icaoidentifier', (req, res) => {
-    axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=tafs&requestType=retrieve&format=xml&stationString=${req.params.icaoidentifier}&hoursBeforeNow=4`).then(xml => {
-        const jsonObj = parser.parse(xml.data);
-        res.json(jsonObj.response.data);
-    }).catch(err => {
-        res.json(err);
-    });
+fastify.get('/metar/:icaoidentifier', async (request, reply) => {
+    const xml = await axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=${request.params.icaoidentifier}&hoursBeforeNow=2`);
+    const jsonObj = parser.parse(xml.data);
+    if (jsonObj.response.data.METAR.length > 0) {
+        return jsonObj.response.data.METAR;    
+    } else {
+        return jsonObj.response;
+    }
 });
 
-app.use(function (req, res) {
-    res.status(404).send('404');
+fastify.get('/taf/:icaoidentifier', async (request, reply) => {
+    const xml = await axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=tafs&requestType=retrieve&format=xml&stationString=${request.params.icaoidentifier}&hoursBeforeNow=4`);
+    const jsonObj = parser.parse(xml.data);
+    return jsonObj.response.data;
 });
 
-app.listen(port, () => console.log('Example app listening on port 3000!'));
+const start = async () => {
+    try {
+        await fastify.listen(port);
+    } catch(err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+}
+start();
