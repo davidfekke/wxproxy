d62f25ddde1c7a2c9ec0d5d5fed0e99cc7576c1f
David Fekke <david@fekke.com>
2021-09-06 20:55:22 -0400

    Adding new XML parsing library for FAA weather data

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import Fastify from 'fastify';
 import fastifyCors from 'fastify-cors';
 import axios from 'axios';
-import * as parser from 'fast-xml-parser';
+//import * as parser from 'fast-xml-parser';
+//import * as parser from 'fastify-xml-body-parser';
+import xml2js from 'xml2js';
 
 const fastify = Fastify({ logger: true });
 
@@ -11,20 +13,23 @@ fastify.register(fastifyCors, {
     // put your options here
 });
 
+//fastify.register(parser);
+
+let parseroptions = {
+    parseNodeValue: true,
+    parseAttributeValue: true
+}
+
 fastify.get('/metar/:icaoidentifier', async (request, reply) => {
     const xml = await axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=${request.params.icaoidentifier}&hoursBeforeNow=2`);
-    const jsonObj = parser.parse(xml.data);
-    if (jsonObj.response.data.METAR.length > 0) {
-        return jsonObj.response.data.METAR;    
-    } else {
-        return jsonObj.response;
-    }
+    const json = await xml2js.parseStringPromise(xml.data, { explicitArray: false, mergeAttrs: true });
+    return json.response.data.METAR;
 });
 
 fastify.get('/taf/:icaoidentifier', async (request, reply) => {
     const xml = await axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=tafs&requestType=retrieve&format=xml&stationString=${request.params.icaoidentifier}&hoursBeforeNow=4`);
-    const jsonObj = parser.parse(xml.data);
-    return jsonObj.response.data;
+    const jsonObj = await xml2js.parseStringPromise(xml.data, { explicitArray: false, mergeAttrs: true });
+    return jsonObj.response.data.TAF;
 });
 
 const start = async () => {
