56672466446c6b6a7b0495f095170f19bc39db64
David Fekke <david@fekke.com>
2024-01-07 20:50:07 -0500

    Removing localhost from swagger config for openapi

--- swaggerConfig.js
+++ swaggerConfig.js
@@ -3,12 +3,9 @@ const swaggerConfig = {
         info: {
           title: 'Aviation Weather Proxy',
           description: 'This is an API for getting aviation weather, specifically METAR and TAFs for pilots.',
-          version: '0.1.0'
+          version: '0.1.1'
         },
         servers: [{
-          url: 'http://localhost:3000'
-        },
-        {
           url: 'https://avwx.fekke.com'
         }]
     },
